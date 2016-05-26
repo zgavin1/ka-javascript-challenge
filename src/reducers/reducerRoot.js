9725a0dee9bcde6414c11789bfc45ca8d917b5dd
Zach Gavin <gavinzach@gmail.com>
2016-05-25 21:15:11 -0400

    Set up reducers for whitelist blacklist and structure.

--- src/reducers/reducerRoot.js
+++ src/reducers/reducerRoot.js
@@ -1,25 +1,85 @@
 import { combineReducers } from 'redux';
 
-const defaultWatchedState = {
-   ForStatement: false,
-   WhileStatement: false,
-   VariableDeclaration: false
+const defaultBlacklist = {whileStatement: false}
+const defaultWhitelist = {forStatement: false}
+const defaultState = {
+   parsedCode: {},
+   blacklist: defaultBlacklist,
+   whitelist: defaultWhitelist
 }
 
-const watched = (
-   state = defaultWatchedState,
-   action = {}
+const whitelist = (
+   state,
+   action
 ) => {
    switch (action.type) {
+      case "FOUND_FOR_STATEMENT":
+         return {forStatement: true};
+      case "NO_FOUND_FOR_STATEMENT":
+         return {forStatement: false};
+      default:
+         return state;
+   }
+}
+
+const blacklist = (
+   state,
+   action
+) => {
+   switch (action.type) {
+      case "FOUND_WHILE_STATEMENT":
+         return {whileStatement: true};
+      case "NO_FOUND_WHILE_STATEMENT":
+         return {whileStatement: false};
+      default:
+         return state;
+   }
+}
+
+const lists = (
+   state = defaultState,
+   action
+) => {
+   switch (action.type) {
+      case "UPDATE_PARSED_CODE":
+         return { 
+            ...state,
+            parsedCode: action.parsedCode
+         };
+      case "FOUND_WHILE_STATEMENT":
+         return { ...state, blacklist: blacklist(undefined, action) };
+      case "FOUND_FOR_STATEMENT":
+         return { ...state, whitelist: whitelist(undefined, action) };
+      case "NO_FOUND_WHILE_STATEMENT":
+         return { ...state, blacklist: blacklist(undefined, action) };
+      case "NO_FOUND_FOR_STATEMENT":
+         return { ...state, whitelist: whitelist(undefined, action) };
       default:
          return state
    }
 }
 
-// I'm calling state watched because I will be watching
-// for these specific inputs, and need to be aware of their presence.
-// The issue I know I'll encounter is updating to false 
-// in the absence of a watched type
+const defaultStructureState = {
+   varDeclNestedInForLoop: false
+}
+
+const structures = (
+   state = defaultStructureState,
+   action
+) => {
+   switch (action.type) {
+      case "FOUND_VARDEC_NESTED_IN_FORLOOP":
+         return {
+            varDeclNestedInForLoop: true
+         }
+      case "NO_FOUND_VARDEC_NESTED_IN_FORLOOP":
+         return {
+            varDeclNestedInForLoop: false
+         }
+      default:
+         return state;
+   }
+}
 
 
-export default combineReducers({watched});
+export default combineReducers({lists, structures});
